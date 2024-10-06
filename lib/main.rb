c618e3229c7ac2cf273fdbbe23ebed902d3ba1ce
Xebros85 <rorban1@live.com>
2024-10-06 07:30:49 -0500

    added start menu support

--- lib/main.rb
+++ lib/main.rb
@@ -12,17 +12,20 @@ class Game
     @secret_word = get_word(dictionary)
     @stages = HangmanStages::STAGES
   end
-  
-  def welcome
+
+  def show_title
     hangman =  RubyFiglet::Figlet.new("Hang Man", 'doom').stringify
-    puts hangman
-    puts
-    puts "Welcome! Choose from the following options:"
+    puts hangman    
+  end
+  
+  def welcome    
+    puts "\n\nChoose from the following options:"
     puts
     show_menu
     puts
     print "Your choice: "    
     @choice = gets.chomp
+    get_choice(@choice) 
   end
   
   def get_word(file)
@@ -30,15 +33,40 @@ class Game
     word.length.between?(5, 12) ? word.upcase : get_word(file)
   end
   
-  def play_game
-    welcome     
+  
+  def show_menu
+    puts "1. Start New Game"
+    puts "2. Load Saved Game"
+    puts "3. Exit"
   end
-end
-
-def show_menu
-  puts "1. Start New Game"
-  puts "2. Load Saved Game"
-  puts "3. Exit"
+  
+  def get_choice(user_choice)
+    case user_choice
+    when "1"      
+      puts "\nStarting new game...\n\n"
+      sleep(1)
+      # start_game      
+    when "2"      
+      puts "\nLoading Game...\n\n"
+      # load game function to load from a YAML file
+      exit
+    when "3"      
+      puts "\nExiting...\n\n"
+      sleep(0.5)      
+    else
+      puts "\nInvalid choice. Please choose from the menu options."
+      sleep(0.75)
+      system("clear")
+      welcome
+    end    
+  end
+  
+  def start_game
+    show_title
+    welcome
+       
+  end
+  
 end
 
 # secret_word = get_word(dictionary)
@@ -46,4 +74,4 @@ end
 # puts "Secret Word Length: #{secret_word.length}"
 
 game = Game.new
-game.play_game
+game.start_game
